681c5db1811e1ebf301ec558b7a7d3ea22e7c4a4
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-01-03 11:10:17 +0700

    Commit my tic-tac-toe project
    This is my first React Project

---
+++ tik-tac-toe/src/App.js
@@ -0,0 +1,29 @@
+function Square({value}) {
+	return (<>
+		<button className="square">{value}</button>
+	</>);
+}
+
+function BoardRow({value1, value2, value3}) {
+	return (<>
+		<div className="board-row">
+			<Square value={value1} />
+			<Square value={value2} />
+			<Square value={value3} />
+		</div>
+	</>);
+}
+
+function Board() {
+	return(<>
+		<BoardRow value1={1} value2={2} value3={3} />
+		<BoardRow value1={4} value2={5} value3={6} />
+		<BoardRow value1={7} value2={8} value3={9} />
+	</>);
+}
+
+export default function Game() {
+	return (<>
+		<Board />
+	</>);
+}
